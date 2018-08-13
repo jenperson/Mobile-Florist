5b3344cfdd36e4425285cf2ef086147e7149c45b
Jen Person <personjeh@gmail.com>
2018-08-13 13:30:00 -0700

    add changes to gitignore

---
+++ GoatLandscaping/GoatLandscaping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoatLandscaping
+//
+//  Created by Jen Person on 8/13/18.
+//  Copyright © 2018 Jen Person. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
